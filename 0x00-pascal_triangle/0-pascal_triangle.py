5ab80c191bc7e76266e1282ee118eeee7b9577c4
MosesChisz <mosesc@cortexware.co.za>
2024-03-06 09:40:05 +0000

    0-pascal_triangle.py

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+    
+    triangle = [[1]]
+    for i in range(1, n):
+        row = [1]  # First element of each row is always 1
+        for j in range(1, i):
+            row.append(triangle[i-1][j-1] + triangle[i-1][j])
+        row.append(1)  # Last element of each row is always 1
+        triangle.append(row)
+    
+    return triangle
+
+# Example usage:
+n = 5
+print(pascal_triangle(n))
